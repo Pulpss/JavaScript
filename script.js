da1b34d389a4528935c4cbfa068ed068a4ca4e1a
Pulpss <45542107+Pulpss@users.noreply.github.com>
2019-03-06 14:17:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function getRandomInt(max){
-  return Math.floor(Math.random() * Math.floor(max));
+	return Math.floor(Math.random() * Math.floor(max));
 }
 function setup() {
-  createCanvas(500, 500);
+	createCanvas(500, 500);
 	background(153);
-	}
+}
 function draw(){
 	fill(color(255, 204, 0));
 	noStroke(); 
 	ellipse(getRandomInt(500), getRandomInt(500), 10, 10)
-	}
+}
